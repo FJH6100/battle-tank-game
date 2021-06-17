3f93ef7d146553d5abeee8ae3faee74b6b862fa9
F.J. Hagen <fredjameshagen@gmail.com>
2021-06-17 00:59:34 -0500

    Completed Tank Movement
    Implemented Tank Movement according to Chapter 1 requirements.

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : Singleton<TankController>
+{
+    public Joystick joystick;
+    float speed = 40f;
+    float horiMove = 0f;
+    float vertiMove = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(joystick.Horizontal * .01f, 0, joystick.Vertical * .01f);
+    }
+}
